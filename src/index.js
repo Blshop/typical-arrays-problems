a37902365a0e2829946338c5bfa379191c8f8afa
Blshop <envworking@gmail.com>
2021-08-22 20:05:29 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  try { if (array[0] == undefined) { return 0 } }
+  catch (error) {
+    return 0
+  }
+  return array.reduce((a, b) => Math.min(a, b));
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  try { if (array[0] == undefined) { return 0 } }
+  catch (error) {
+    return 0
+  }
+  return array.reduce((a, b) => Math.max(a, b));
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  try { if (array[0] == undefined) { return 0 } }
+  catch (error) {
+    return 0
+  }
+  return array.reduce((a, b) => a + b) / array.length;
 }
